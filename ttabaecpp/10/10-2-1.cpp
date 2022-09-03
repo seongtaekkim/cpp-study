c9ede7d1ccecac0212b2affb3eca1e136228bd79
seongtaekkim <chxortnl@gmail.com>
2022-09-04 08:47:00 +0900

    rename file

--- ttabaecpp/10/10-2-1.cpp
+++ ttabaecpp/10/10-2-1.cpp
@@ -1,36 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include "Lecture.h"
+#include "Monster.h"
 
 int main(void)
 {
-    using namespace std;
-    
-    Teacher t1("prof. hong");
-    Teacher t2("prof. taek");
-    Student s1("kim1", 0);
-    Student s2("kim2", 1);
-    Student s3("kim3", 2);
-
-    Lecture lec1("introduction to computer programming");
-    lec1.assignTeacher(t1);
-    lec1.registerStudent(&s1);
-    lec1.registerStudent(&s2);
-    lec1.registerStudent(&s3);
-
-    Lecture lec2("computational thingking");
-    lec2.assignTeacher(t2);
-    lec2.registerStudent(&s1);
-
-    {
-        cout << lec1 << endl;
-        cout << lec2 << endl;
-
-        lec2.study();
-
-        cout << lec1 << endl;
-        cout << lec2 << endl;
-    }
+    Monster m1("staek", Position2D(1,1));
+    std::cout << m1 << std::endl;
+    m1.moveTo(Position2D(4,4));
+    std::cout << m1 << std::endl;
     return (0);
 }
